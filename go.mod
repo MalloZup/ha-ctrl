b5f39ac8bd0540e588511a6ed49904dc8d15acd9
dmaiocchi <dmaiocchi@suse.com>
2020-08-29 00:31:09 +0200

    Refactor code for pacemaker process

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module MalloZup/ha-ctrl
 go 1.13
 
 require (
+	github.com/briandowns/spinner v1.11.1
 	github.com/mitchellh/go-ps v1.0.0
 	github.com/shirou/gopsutil v2.20.7+incompatible
 	github.com/sirupsen/logrus v1.6.0
--- main.go
+++ main.go
@@ -5,38 +5,18 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func checkPacemakerProcesses(proc *ps.Process) {
-	pacemakerProcNames := []string{
+func checkPacemakerProcesses() {
+
+	pacemakerProcNames := map[string]bool{
 		// this might be different across versions..
-		"/usr/sbin/pacemakerd",
-		"/usr/lib/pacemaker/pacemaker-based",
-		"/usr/lib/pacemaker/pacemaker-fenced",
-		"/usr/lib/pacemaker/pacemaker-execd",
-		"/usr/lib/pacemaker/pacemaker-attrd",
-		"/usr/lib/pacemaker/pacemaker-schedulerd",
-		"/usr/lib/pacemaker/pacemaker-controld",
+		"/usr/sbin/pacemakerd":                    false,
+		"/usr/lib/pacemaker/pacemaker-based":      false,
+		"/usr/lib/pacemaker/pacemaker-fenced":     false,
+		"/usr/lib/pacemaker/pacemaker-execd":      false,
+		"/usr/lib/pacemaker/pacemaker-attrd":      false,
+		"/usr/lib/pacemaker/pacemaker-schedulerd": false,
+		"/usr/lib/pacemaker/pacemaker-controld":   false,
 	}
-	procExec, _ := proc.Exe()
-	log.Debugf("%d\t%s\n", proc.Pid, procExec)
-	// go trough the list of our process and check their status
-	for _, p := range pacemakerProcNames {
-		if procExec == p {
-			procStatus, _ := proc.Status()
-			log.Infof("%d\t%s\t%s\n", proc.Pid, procExec, procStatus)
-			// R: Running S: Sleep T: Stop I: Idle
-			// Z: Zombie W: Wait L: Lock
-			// tollerate running and sleeping. Otherwise print warning
-			if procStatus != "R" && procStatus != "S" {
-				log.Warnf("Process %s\t state is in a not expected status: %d\t%s\n", procExec, proc.Pid, procStatus)
-			}
-		}
-	}
-
-}
-
-func main() {
-
-	log.Info("Starting ha-control..")
 
 	processList, err := ps.Processes()
 	if err != nil {
@@ -48,11 +28,37 @@ func main() {
 		proc := processList[x]
 		procExec, _ := proc.Exe()
 		log.Debugf("%d\t%s\n", proc.Pid, procExec)
-		// go trough the list
-		checkPacemakerProcesses(proc)
 
+		// go trough the list of our process and check their status
+		for key := range pacemakerProcNames {
+			if procExec == key {
+				// process expected was found
+				pacemakerProcNames[procExec] = true
+
+				procStatus, _ := proc.Status()
+				log.Infof("%d\t%s\t%s\n", proc.Pid, procExec, procStatus)
+				// R: Running S: Sleep T: Stop I: Idle  Z: Zombie W: Wait L: Lock
+				// tollerate running and sleeping. Otherwise print warning
+				if procStatus != "R" && procStatus != "S" {
+					log.Warnf("Process %s\t state is in a not expected status: %d\t%s\n", procExec, proc.Pid, procStatus)
+				}
+			}
+		}
 	}
 
-	log.Info("Health check sucessfully completed!")
+	// check if we don't have some process not active
+	for key, value := range pacemakerProcNames {
+		if value == false {
+			log.Errorf("Process %s needed for HA was not found", key)
+		}
+	}
+}
 
+func main() {
+
+	log.Info("Starting ha-control..")
+
+	// 01) Check process
+	log.Infoln("Checking relavant HA Cluster process")
+	checkPacemakerProcesses()
 }
