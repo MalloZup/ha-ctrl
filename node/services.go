14e28f69714f6797dd21466078720206057338d1
dmaiocchi <dmaiocchi@suse.com>
2020-08-29 17:23:44 +0200

    add Info when service is ok

--- node/services.go
+++ node/services.go
@@ -27,5 +27,6 @@ func systemctlStatus(service string) error {
 		log.Errorf("service %s is not running correctly. %s \n error %s", service, out, err)
 		return err
 	}
+	log.Infof("service %s is up and running", service)
 	return nil
 }
